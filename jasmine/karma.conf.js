3ff8cb09a74c7a4517af8e62e5a93b8540432f72
Collins Wekesa <collins@Collinss-MacBook-Pro.local>
2017-01-03 22:30:55 +0300

    chore:change browser

--- jasmine/karma.conf.js
+++ jasmine/karma.conf.js
@@ -59,8 +59,8 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    // browsers: ['Chrome'],
-    browsers: ['PhantomJS2'],
+     browsers: ['Chrome'],
+    //browsers: ['PhantomJS2'],
 
 
     // Continuous Integration mode
