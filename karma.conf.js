d50026d2a5d4b84aedef2334ea763cdee9599d3f
Collins Wekesa <collins.wekesa@andela.com>
2017-01-15 13:52:21 +0300

    karma minor bug fix

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'jasmine/spec/*.js'
+      'jasmine/spec/*.js',
       'public/src/*.js',
     ],
 
