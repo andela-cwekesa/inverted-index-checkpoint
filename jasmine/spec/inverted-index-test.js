aa93af57a375ca378098b76bdb4accd7f21b64d1
Collins Wekesa <collins.wekesa@andela.com>
2017-01-18 19:42:18 +0300

    add 2 spaces

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -15,61 +15,61 @@ const testData =  [
 const obj = new Index();
 describe("Read book data", () => {
     it("verifies that JSON file passed is not empty",  () => {
-        expect(testData.length > 0).toBeTruthy();
+      expect(testData.length > 0).toBeTruthy();
     }); 
     it("should verify actually a file to be indexed is loaded" ,  () => {
-        expect(testData).toBeTruthy();
+      expect(testData).toBeTruthy();
     });
     it("should ensure that the file content is actually a valid JSON Array" ,   () => {
-        expect((testData instanceof Array)).toBeTruthy();
+      expect((testData instanceof Array)).toBeTruthy();
     });
     it("should ensure that each object in JSON array contains a property whose value is a string" ,  () => {
     	testData.forEach((element) =>{
-    		expect(typeof element.title === "string").toBeTruthy();
-    		expect(typeof element.text  === "string").toBeTruthy();
+    	expect(typeof element.title === "string").toBeTruthy();
+    	expect(typeof element.text  === "string").toBeTruthy();
     	});
     }); 
     it("should ensure the json file is in good format" ,  () => {
-        let goodFormat = eval(testData);
-        expect(goodFormat).toBeTruthy();
+      let goodFormat = eval(testData);
+      expect(goodFormat).toBeTruthy();
     });
 });
 // Populate Index test suite
 describe("Populate Index" , () => {  
     it("should ensure index is created once JSON file has been read",  () => {
         // file object 
-        let jsonfile = {
-            name: "testFile",
-            files: testData[0]
-        };     
-        let created = obj.createIndex(jsonfile);
-        expect(created.message).toBe(jsonfile.name + " has been indexed successfully.")
+      let jsonfile = {
+        name: "testFile",
+        files: testData[0]
+      };     
+      let created = obj.createIndex(jsonfile);
+      expect(created.message).toBe(jsonfile.name + " has been indexed successfully.")
     });
 });
 describe("Check existence" , () => {
-    it("should check that searchIndex method exists" , () => {
-        let searchy = obj.searchIndex();
-        expect(searchy).toBeTruthy();
-    });
-    it("should check that getIndex method exists" , () => {    
-        let gety = obj.getIndex();
-        expect(gety).toBeTruthy();
-    });
+  it("should check that searchIndex method exists" , () => {
+    let searchy = obj.searchIndex();
+    expect(searchy).toBeTruthy();
+  });
+  it("should check that getIndex method exists" , () => {    
+    let gety = obj.getIndex();
+    expect(gety).toBeTruthy();
+  });
 }); 
 // Search index test suite
 // I have used a custom jasmine matcher
 describe("Search index" , () => {
-    beforeEach(() => {
-        let hits = []; // initialize an empty array that will hold search hits
-        jasmine.addMatchers({
-        toHaveSomething: () =>{
-            hits.length >0;
-        }
-    })
+  beforeEach(() => {
+    let hits = []; // initialize an empty array that will hold search hits
+    jasmine.addMatchers({
+      toHaveSomething: () =>{
+        hits.length >0;
+      }
     });
-    it("test that verifies that searching the index returns an array of the indices.", () =>{
+  });
+  it("test that verifies that searching the index returns an array of the indices.", () =>{
         
-        hitz = obj.searchIndex(testData);
-        expect(hitz).toHaveSomething;
-    });    
+    let hitz = obj.searchIndex(testData);
+    expect(hitz).toHaveSomething;
+  });    
 });
