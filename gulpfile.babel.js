c0713bfbf7d19c63d6cb36058278f44d2f29eae8
Collins Wekesa <collins.wekesa@andela.com>
2017-01-18 10:30:52 +0300

    edit comment and remove single quotes

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,32 +1,32 @@
 /**
  * This file is dependent on gulp which automate tasks.
- *Author : # Collins
+ * Author : # Collins
  */
-const gulp = require('gulp'),
-    less = require('gulp-less'),
-    livereload = require('gulp-livereload'),
-    cleanCSS = require('gulp-clean-css'),
-    jshint = require('gulp-jshint');
+const gulp = require("gulp"),
+    less = require("gulp-less"),
+    livereload = require("gulp-livereload"),
+    cleanCSS = require("gulp-clean-css"),
+    jshint = require("gulp-jshint");
 const browserSync1 = require("browser-sync").create();
 const browserSync2 = require("browser-sync").create();
 const browserSync = require("browser-sync").create();
 gulp.task("default", ["front","test"]);
-gulp.task('css', () => {
-  gulp.src('public/design/css/*.css')
+gulp.task("css", () => {
+  gulp.src("public/design/css/*.css")
     .pipe(cleanCSS())
-    .pipe(gulp.dest('css'))
+    .pipe(gulp.dest("css"))
     .pipe(livereload(".public/index.html"));
 }); 
-gulp.task('watch', () => {
+gulp.task("watch", () => {
   livereload.listen();
-  gulp.watch('public/design/css/*.css', ['css']);
+  gulp.watch("public/design/css/*.css", ["css"]);
 });
 // A task that ensures the javascript task is complete before reloading browsers
-gulp.task('js-watch', ['js'],  (done) => {
+gulp.task("js-watch", ["js"],  (done) => {
     browserSync.reload();
     done();
 });
-gulp.task('front',  () => {
+gulp.task("front",  () => {
     browserSync1.init({
         server : {
             baseDir: "./public"
@@ -42,12 +42,12 @@ gulp.task('front',  () => {
 gulp.task("test",  () => {
     browserSync2.init({
         server: {
-          baseDir: ["./public/src/", "./jasmine"]},
-          port: 3080,
-          ui: {
-              port: 3090
-          }
+            baseDir: ["./public/src/", "./jasmine"]},
+        port: 3080,
+        ui: {
+            port: 3090
+        }
     });
-    gulp.watch('./jasmine/spec/*.js').on("change",browserSync2.reload);
+    gulp.watch("./jasmine/spec/*.js").on("change",browserSync2.reload);
     gulp.watch("./public/src/*.js").on("change", browserSync2.reload);
 }); 
