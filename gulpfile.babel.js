ab64d97511d8b59e69f7d3c8ed579762d5507060
Collins Wekesa <collins.wekesa@andela.com>
2017-01-18 19:47:45 +0300

    add two spaces

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,11 +3,11 @@
  * Author : # Collins
  */
 const gulp = require("gulp"),
-    less = require("gulp-less"),
-    livereload = require("gulp-livereload"),
-    cleanCSS = require("gulp-clean-css"),
-    jasmine = require("gulp-jasmine"),
-    jshint = require("gulp-jshint");
+  less = require("gulp-less"),
+  livereload = require("gulp-livereload"),
+  cleanCSS = require("gulp-clean-css"),
+  jasmine = require("gulp-jasmine"),
+  jshint = require("gulp-jshint");
 const browserSync1 = require("browser-sync").create();
 const browserSync2 = require("browser-sync").create();
 const browserSync = require("browser-sync").create();
@@ -24,36 +24,36 @@ gulp.task("watch", () => {
 });
 // A task that ensures the javascript task is complete before reloading browsers
 gulp.task("js-watch", ["js"],  (done) => {
-    browserSync.reload();
-    done();
+  browserSync.reload();
+  done();
 });
 gulp.task("front",  () => {
-    browserSync1.init({
-        server : {
-            baseDir: "./public"
-        },
-        port : 3002,
-        ui: {
-            port:3070
-        }
-    });
+  browserSync1.init({
+    server : {
+      baseDir: "./public"
+    },
+    port : 3002,
+    ui: {
+      port:3070
+    }
+  });
     // Tracks html,js and css changes.I have used to allow reload
-    gulp.watch("./public/**/*.{html,js,css}").on("change", browserSync1.reload);
+  gulp.watch("./public/**/*.{html,js,css}").on("change", browserSync1.reload);
 });
 gulp.task("test1",  () => {
-    browserSync2.init({
-        server: {
-            baseDir: ["./public/src/", "./jasmine"]},
-        port: 3080,
-        ui: {
-            port: 3090
-        }
-    });
-    gulp.watch("./jasmine/spec/*.js").on("change",browserSync2.reload);
-    gulp.watch("./public/src/*.js").on("change", browserSync2.reload);
+  browserSync2.init({
+    server: {
+      baseDir: ["./public/src/", "./jasmine"]},
+    port: 3080,
+    ui: {
+      port: 3090
+    }
+  });
+  gulp.watch("./jasmine/spec/*.js").on("change",browserSync2.reload);
+  gulp.watch("./public/src/*.js").on("change", browserSync2.reload);
 }); 
 
 gulp.task("test" , () => {
-    gulp.src("./jasmine/spec/inverted-index-test.js")
+  gulp.src("./jasmine/spec/inverted-index-test.js")
     .pipe(jasmine());
 });
