4834c45064b2a869a328f7bde3760914ccbb0c51
Collins Wekesa <collins.wekesa@andela.com>
2017-01-22 21:30:28 +0300

    remove doubles quotes as per airbnb

--- public/angularjs/file.js
+++ public/angularjs/file.js
@@ -1,25 +1,25 @@
-let app = angular.module("myApp", []);
-app.directive("fileChange", ["$window",  ($window) => {
+let app = angular.module('myApp', []);
+app.directive('fileChange', ['$window',  ($window) => {
     return {
         // both Element(E) names and Attribute(A) names can invoke the directive
-        restrict: "EA",
-        require: "ngModel",
+        restrict: 'EA',
+        require: 'ngModel',
         link:  (scope, element, attr, control) => {
             // FileReader is used to read the contents of a file 
             const reader = new FileReader;
             // get access to element that triggered an event
-            element.bind("change",  (fc) => {    
+            element.bind('change',  (fc) => {    
                 let fileContents = fc.target.files[0];  // Retrieves the first File from the FileList object (w3c file api)
-                if (fileContents.name.indexOf("json") >= 0) {
+                if (fileContents.name.indexOf('json') >= 0) {
                     file = fileContents.name;
                 // reading data in the file    
                 let check = reader.readAsText(fileContents);
                 } 
                 else if (!fileContents){
-                    $window.alert ("Unable to load the file.Please retry");
+                    $window.alert ('Unable to load the file.Please retry');
                 }
                 else {
-                    $window.alert("Please , strictly select a JSON file.");
+                    $window.alert('Please , strictly select a JSON file.');
                 }              
             });
             reader.onload = () => {
@@ -35,7 +35,7 @@ app.directive("fileChange", ["$window",  ($window) => {
         }
     };
 }]); 
-app.controller("myController" ,  ($scope , $timeout) => {
+app.controller('myController' ,  ($scope , $timeout) => {
     $scope.beforeIndex = {}; 
     $scope.container = {};
     const obj = new Index();   
@@ -51,7 +51,7 @@ app.controller("myController" ,  ($scope , $timeout) => {
             JSON.parse(x);
             let success = obj.createIndex(fileContents);   
             if (success) {
-                alert ("The operation was successful.");
+                alert ('The operation was successful.');
                 let result = obj.getIndex();
                 let fileLength = result[fileContents.name].uploadedFile;
                 $timeout( () => {
@@ -67,18 +67,18 @@ app.controller("myController" ,  ($scope , $timeout) => {
                     };
                 }, 200);
             } else {
-                alert("The operation was not successful.");
+                alert('The operation was not successful.');
             }
-        }catch(e){alert("Invalid JSON file");}
+        }catch(e){alert('Invalid JSON file');}
     };
     $scope.searchIndex =  () => {
         let fName = $scope.selectedFile;
         let txtSearch = $scope.txtSearch;
         if (!txtSearch) {
-            alert("You haven\'t provided anything to be searched.");
+            alert('You haven\'t provided anything to be searched.');
         }
         else if (Object.keys($scope.beforeIndex).length <= 0) {
-            alert("Unable to find an indexed file");
+            alert('Unable to find an indexed file');
         }
         else{
             $scope.searchResults = obj.searchIndex(fName , txtSearch);
